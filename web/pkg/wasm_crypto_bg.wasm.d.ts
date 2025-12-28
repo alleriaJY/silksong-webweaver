8420f623b2a07c83ddb1a5c12921798b11e8b5f3
JunYuan Tan <junyuan98@hotmail.com>
2025-12-28 21:39:07 +0800

    Add prebuilt WASM package
    Probably will change to build it in Github Actions later on. Will
    see what happens in future.
    
    Signed-off-by: JunYuan Tan <junyuan98@hotmail.com>

---
+++ web/pkg/wasm_crypto_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const decrypt_and_parse: (a: number, b: number) => any;
+export const init: () => void;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_start: () => void;
